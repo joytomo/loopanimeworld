a6d1e2e49cc91fcded0dae6f4e32c5cbb01996e8
Tomoya Hayashida <joyfulltomoya@gmail.com>
2018-11-18 22:41:15 +0900

    add initial content

--- js/index.js
+++ js/index.js
@@ -2,17 +2,23 @@ var count = 0;
 var screen_width = window.parent.screen.width;
 
 window.onload = function() {
-  setInterval('unchi()', 1);
+  setInterval('counter()', 10);
+  setInterval('slide_start("right")', 10);
+  setInterval('slide_start("left")', 10);
 }
 
-function unchi() {
+function counter() {
   count += 1;
-  $('#scroll_1').css({
-    "margin-left": count     
-  });
-  if(count >= screen_width*2) {
+  if(count >= screen_width*1.5) {
     count = 0;
   }
+}
+
+function slide_start(direction) {
+  move = direction == 'right' ? count : -count;
+  $('.slider-' + direction).css({
+      "margin-left": move
+  });
 }
 
 window.addEventListener("scroll", function() {
@@ -29,10 +35,19 @@ window.addEventListener("scroll", function() {
   if (window.parent.screen.width > 768){
     artist_move('artist0', 'left');
     artist_move('artist1', 'right');
+    artist_move('artist2', 'left');
+    artist_move('artist3', 'right');
+    artist_move('artist4', 'left');
+    artist_move('artist5', 'right');
+    artist_move('artist6', 'left');
   } else {
     smartphone_margin('artist0');
     smartphone_margin('artist1');
-
+    smartphone_margin('artist2');
+    smartphone_margin('artist3');
+    smartphone_margin('artist4');
+    smartphone_margin('artist5');
+    smartphone_margin('artist6');
   }
 
   function artist_move(artist_id, direction) {
@@ -41,14 +56,14 @@ window.addEventListener("scroll", function() {
     if (direction == 'left') {
       if (clientRect.top - window.parent.screen.height < 0) {
         $('#' + artist_id).css({
-          "margin-left": clientRect.top
+          "margin-left": -window.parent.screen.width*0.1+clientRect.top
         });
       }
     }
     if (direction == 'right') {
       if (clientRect.top - window.parent.screen.height < 0) {
         $('#' + artist_id).css({
-          "margin-left": window.parent.screen.width*0.3-clientRect.top
+          "margin-left": window.parent.screen.width*0.1-clientRect.top
         });
       }
     }
